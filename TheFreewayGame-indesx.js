8cc0bec2db4f6d5f08fc452195f1b1ad52582df0
Sulaiman Marey <83997567+sulaiman211@users.noreply.github.com>
2021-12-30 02:31:39 -0700

    Add files via upload

---
+++ TheFreewayGame-indesx.js
@@ -0,0 +1,51 @@
+/*
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Sydney_-_Newcastle_freeway_north_bound_at_Berowra.jpg/500px-Sydney_-_Newcastle_freeway_north_bound_at_Berowra.jpg">
+
+
+# Back-Story
+
+Every day I travel on the freeway.
+
+When I am more bored than usual I sometimes like to play the following counting game I made up:
+
+* As I join the freeway my count is ```0```
+* Add ```1``` for every car that I overtake
+* Subtract ```1``` for every car that overtakes me
+* Stop counting when I reach my exit
+
+What an easy game! What fun!
+
+# Kata Task
+
+You will be given
+* The distance to my exit (km)
+* How fast I am going (kph)
+* Information about a lot of other cars 
+ * Their time (relative to me) as I join the freeway. For example,
+   * ```-1.5``` means they already passed my starting point ```1.5``` minutes ago
+   * ```2.2``` means they will pass my starting point ```2.2``` minutes from now
+ * How fast they are going (kph)
+
+Find what is my "score" as I exit the freeway!
+
+# Notes
+
+* Assume all cars travel at a constant speeds
+
+<span style='font-size:25px;font-weight:bold;color:red;background:yellow;border:solid orange 5px;'>&nbsp;Safety Warning&nbsp;</span>
+
+If you plan to play this "game" remember that it is not really a game. You are in a **real** car.
+
+There may be a temptation to try to beat your previous best score.
+*/
+
+const freewayGame = function(distKmToExit, mySpeedKmPH, otherCars) {
+    let myTimeToExit = distKmToExit / mySpeedKmPH;
+    return otherCars.reduce(
+      (val, [lead, speed]) =>
+        val +
+        (lead < 0 && myTimeToExit * speed < distKmToExit + (lead / 60) * speed) -
+        (lead > 0 && myTimeToExit * speed > distKmToExit + (lead / 60) * speed),
+      0
+    );
+  };
